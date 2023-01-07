c5739dbd463c074bf24911943607a76a36b7742b
Vincent St-Pierre <vstpierre1@gmail.com>
2023-01-06 21:39:23 -0500

    SnailController update

--- Assets/Script/SnailController.cs
+++ Assets/Script/SnailController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class SnailController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    [Header("Movement")]
+    [SerializeField]
+    float speed = 2f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+
+    }
+
+    void HandleMovement()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        if(horizontalMove != 0 && verticalMove != 0)
+        {
+            Vector3 move = transform.forward * verticalMove + transform.right * horizontalMove;
+            controller.Move(speed * Time.deltaTime * move);
+
+        }
     }
 }
