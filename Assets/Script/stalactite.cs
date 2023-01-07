fdf1627dceb1e13f20eb4a80e9f0344012a5db64
Hugo Morencé <torkarra@gmail.com>
2023-01-06 22:07:28 -0500

    Stalactite

---
+++ Assets/Script/stalactite.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stalactite: MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float distanceFall;
+    [SerializeField] private Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        rigid.useGravity = false;
+    }
+
+    void Update()
+    {
+        if((transform.position.x - player.transform.position.x) < distanceFall)
+        {
+            rigid.useGravity = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
