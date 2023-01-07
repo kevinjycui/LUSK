90b903a8aae7cf1c7944e0a6e7757c39198c7521
Hugo Morencé <torkarra@gmail.com>
2023-01-06 21:41:03 -0500

    GameManager started

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Season")]
+    [SerializeField, HideInInspector] private float timeNextSeason = 0f;
+    [SerializeField, HideInInspector] private float timeInterval = 600f;
+    [SerializeField, HideInInspector] private int whichSeason = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeNextSeason < Time.time)
+        {
+            if(whichSeason == 0)
+            {
+                Spring();
+            }
+            else if (whichSeason == 1)
+            {
+                Summer();
+            }
+            else if (whichSeason == 2)
+            {
+                Fall();
+            }
+            else if (whichSeason == 3)
+            {
+                Winter();
+            }
+            whichSeason = (whichSeason + 1) % 4;
+            timeNextSeason += timeInterval;
+        }
+    }
+
+    void Spring()
+    {
+
+    }
+
+    void Summer()
+    {
+
+    }
+
+    void Fall()
+    {
+
+    }
+
+    void Winter()
+    {
+
+    }
+}
