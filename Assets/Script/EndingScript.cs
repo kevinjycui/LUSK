70670079a5dae46e19492992e485e62cfc802383
Hugo Morencé <torkarra@gmail.com>
2023-01-08 14:29:16 -0500

    kettle and ending

---
+++ Assets/Script/EndingScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingScript : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] CanvasGroup blackScreen;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        TheEnd();
+    }
+
+    IEnumerator TheEnd()
+    {
+        while(player.position.z < 70f)
+        {
+            player.Translate(0f, 0.5f, 2f);
+        }
+
+        yield return new WaitForSeconds(2f);
+
+        while (blackScreen.alpha < 1f)
+        {
+            blackScreen.alpha += 0.005f;
+            yield return 5f;
+        }
+
+        yield return new WaitForSeconds(5f);
+
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Script/Kettle.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kettle : MonoBehaviour
+{
+    [SerializeField] GameObject steamEffect;
+    [SerializeField] bool isActive = false;
+    [SerializeField] bool isLoaded = false;
+    [SerializeField] GameObject spiderWeb;
+    [SerializeField] GameObject leaf;
+    [SerializeField] FieldManager fm;
+    void Start()
+    {
+        steamEffect.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        if (isActive)
+        {
+            Steaming();
+        }
+        LoadedAndSummer();
+    }
+
+    IEnumerator Steaming()
+    {
+        steamEffect.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        GameObject.Destroy(spiderWeb);
+    }
+
+    void OnCollisionEnter(Collider target)
+    {
+        if (target.gameObject.tag.Equals("leaf") == true)
+        {
+            GameObject.Destroy(leaf);
+            isLoaded = true;
+        }
+    }
+    
+    void LoadedAndSummer()
+    {
+        //&& fm.season == 1
+        if (isLoaded)
+        {
+            isActive = true;
+        }
+    }
+}
